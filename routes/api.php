afbd4db4adbbcbc49645db23a8d08052b3b8e388
reyvandresta <reyvandresta77@gmail.com>
2025-03-14 14:09:23 +0700

    tabel book

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\bookController;
 
 // Menggunakan apiResource untuk resource CRUD standar
 Route::apiResource('users', userController::class);
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('books', bookController::class);
 // Route contoh untuk mengambil user yang sudah autentikasi menggunakan Sanctum
