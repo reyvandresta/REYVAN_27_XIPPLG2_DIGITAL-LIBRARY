fdc650f9a6263aad942df7905414c2cbafde8750
reyvandresta <reyvandresta77@gmail.com>
2025-03-14 14:00:35 +0700

    tabel category

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 // Menggunakan apiResource untuk resource CRUD standar
 Route::apiResource('users', userController::class);
+Route::apiResource('categories', CategoryController::class);
 // Route contoh untuk mengambil user yang sudah autentikasi menggunakan Sanctum
