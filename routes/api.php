c4856224b50d7852c634547760281c6c3269fa67
reyvandresta <reyvandresta77@gmail.com>
2025-03-14 14:38:44 +0700

    tabel review

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\bookController;
+use App\Http\Controllers\ReviewsController;
 
 // Menggunakan apiResource untuk resource CRUD standar
 Route::apiResource('users', userController::class);
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('books', bookController::class);
+Route::apiResource('reviews', ReviewsController::class);
 // Route contoh untuk mengambil user yang sudah autentikasi menggunakan Sanctum
